aab1a5d3bab38144243b9b01edaeff0e59379a27
Giedrekor <43093161+Giedrekor@users.noreply.github.com>
2024-12-08 22:33:33 +0200

    Day 8

--- december_8/script.js
+++ december_8/script.js
@@ -5,16 +5,29 @@ const houses = [
 ];
 const speed = 10; // in km/h
 let totalTime = 0; // in h
-let distance = 0; // in km
+let totalDistance = 0; // in km
+const home = [0, 0];
 
 function calculateDeliveryTime(houses, speed) {
-  // to [1, 2, 3, 4, 5, 6], then calculate sum and double distance
-  houses.flat().forEach((x) => {
-    distance += x;
-    totalTime = (distance / speed) * 2;
-  });
+  // distance to next stop
+  let stop1 = Math.sqrt(Math.pow(houses[0][0], 2) + Math.pow(houses[0][1], 2));
+  let stop2 = Math.sqrt(
+    Math.pow(houses[1][0] - houses[0][0], 2) +
+      Math.pow(houses[1][1] - houses[0][1], 2)
+  );
+  let stop3 = Math.sqrt(
+    Math.pow(houses[2][0] - houses[1][0], 2) +
+      Math.pow(houses[2][1] - houses[1][1], 2)
+  );
+  // distance to home
+  let stopHome = Math.sqrt(
+    Math.pow(houses[2][0] - home[0], 2) + Math.pow(houses[2][1] - home[1], 2)
+  );
+
+  totalDistance = (stop1 + stop2 + stop3 + stopHome).toFixed(2);
+  totalTime = (totalDistance / speed).toFixed(1);
   return totalTime;
 }
 
 console.log(calculateDeliveryTime(houses, speed));
-// console.log('Distance ' + distance + 'km, total Time ' + totalTime + 'hours.');
+// console.log('Total distance: ' + totalDistance + 'km, total time: ' + totalTime + 'hours.');
